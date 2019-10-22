19d5c7417e55a9f08e8102fbb15f68aeac73e4a2
yairg <yjgarizabalo@gmail.com>
2019-10-21 23:36:01 -0500

    upload motor de template  and views

--- src/server/config.js
+++ src/server/config.js
@@ -12,11 +12,11 @@ module.exports = app => {
 
   //setings
   app.set('port', process.env.PORT || 7000)
-  app.set('views', path.join(__dirname, 'views'))
+  app.set('views', path.join(__dirname, '../views'))
   app.engine('.hbs', exphbs({
     defaultLayout: 'main',
     partialsDir: path.join(app.get('views'), 'partials'),
-    layoutsDir: path.join(app.get('views'), 'layoouts'),
+    layoutsDir: path.join(app.get('views'), 'layouts'),
     extname: '.hbs',
     helpers: require('./helpers')
   }))
