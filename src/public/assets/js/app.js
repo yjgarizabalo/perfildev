1b4716f5256dd4215c9f20d9b40c8d3d56fa66fc
Yair G <Ygarizabalo@gmail.com>
2020-04-26 22:23:11 -0500

    update perfildev

--- src/public/assets/js/app.js
+++ src/public/assets/js/app.js
@@ -3,15 +3,15 @@
 let buscador =  new Search({
     el: '#bs1',
     list: [
-        '👨‍💻 <a class="text-decoration-none text-body" href="/">Desarrollador web</a>',
-        '👨‍💻 <a class="text-decoration-none text-body" href="/">Programador back-end</a>',
-        '👨‍💻 <a class="text-decoration-none text-body" href="/">Programador front-end</a>',
-        '👨‍💼 <a class="text-decoration-none text-body" href="/">Ingenidero de sistemas</a>',
-        '👨‍🏫 <a class="text-decoration-none text-body" href="/">Analista de sistemas</a>', 
-        '👨‍🔧 <a class="text-decoration-none text-body" href="/">Tec. de mantenimiento de computadores</a>',
-        '👷 <a class="text-decoration-none text-body" href="/">Tecnico Electronico</a>',
-        '👨‍🎨 <a class="text-decoration-none text-body" href="/">Diseñador grafico</a>',
-        '📷 <a class="text-decoration-none text-body" href="/">Fotografo</a>',
-        '🎯 <a class="text-decoration-none text-body" href="/">Otro</a>'   
+        '👨‍💻 <a class="text-decoration-none text-body" href="/signin">Desarrollador web</a>',
+        '👨‍💻 <a class="text-decoration-none text-body" href="/signin">Programador back-end</a>',
+        '👨‍💻 <a class="text-decoration-none text-body" href="/signin">Programador front-end</a>',
+        '👨‍💼 <a class="text-decoration-none text-body" href="/signin">Ingenidero de sistemas</a>',
+        '👨‍🏫 <a class="text-decoration-none text-body" href="/signin">Analista de sistemas</a>', 
+        '👨‍🔧 <a class="text-decoration-none text-body" href="/signin">Tec. de mantenimiento de computadores</a>',
+        '👷 <a class="text-decoration-none text-body" href="/signin">Tecnico Electronico</a>',
+        '👨‍🎨 <a class="text-decoration-none text-body" href="/signin">Diseñador grafico</a>',
+        '📷 <a class="text-decoration-none text-body" href="/signin">Fotografo</a>',
+        '🎯 <a class="text-decoration-none text-body" href="/signin">Otro</a>'   
     ]
 });
