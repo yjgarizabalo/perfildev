25fdf18e8665a511186ccb95bc748e0b0e6fa715
yairG <yjgarizabalo@gmail.com>
2020-05-05 23:00:37 -0500

    update header

--- src/public/assets/js/main.js
+++ src/public/assets/js/main.js
@@ -22,22 +22,22 @@ window.addEventListener('resize', () => {
 
 // dropdown
 
-function dropbtn_perfildev() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
+// function dropbtn_perfildev() {
+//     document.getElementById("myDropdown").classList.toggle("show");
+//   }
   
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
+//   // Close the dropdown if the user clicks outside of it
+//   window.onclick = function(event) {
+//     if (!event.target.matches('.dropbtn')) {
+//       var dropdowns = document.getElementsByClassName("dropdown-content");
+//       var i;
+//       for (i = 0; i < dropdowns.length; i++) {
+//         var openDropdown = dropdowns[i];
+//         if (openDropdown.classList.contains('show')) {
+//           openDropdown.classList.remove('show');
+//         }
+//       }
+//     }
+//   }
 
 // end dropdown
