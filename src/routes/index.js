35cad3b8b3f713918e3fed3af4a8f80a0b5e1219
yairG <yjgarizabalo@gmail.com>
2020-05-11 18:35:17 -0500

    update index  add underconstruction

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.render('index.hbs')
+    // res.render('index.hbs')
+    res.render('build/enconstruccion.hbs')
 })
 
 module.exports = router
